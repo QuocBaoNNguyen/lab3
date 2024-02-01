8067cc4857d632bac93fec043dad8a6e930cfbe7
QuocBaoNNguyen <156359008+QuocBaoNNguyen@users.noreply.github.com>
2024-02-01 11:26:08 -0800

    saving array test.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,35 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
+  @Test
+  public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
-
+    assertArrayEquals(new int[] { 3 }, input1);
+  }
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {};
+    assertArrayEquals(new int[] {}, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedNonEmpty() {
+    int[] input1 = { 1, 2, 3, 4 };
+    assertArrayEquals(new int[] { 4, 3, 2, 1 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlaceMultiple() {
+    int[] input1 = { 1, 2, 3, 4 };
+    assertArrayEquals(new int[] { 4, 3, 2, 1 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAvgWithoutLowestWithSomeSameValues() {
+    double[] input1 = { 1.0, 5.0, 5.0, 5.0 };
+    assertEquals(10, ArrayExamples.averageWithoutLowest(input1), 5);
+  }
+
 }
